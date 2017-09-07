cf5a4bc18f453adec7fca5c0bb5f43f4111d0815
Rich T <rtranter25@gmail.com>
2017-09-07 10:11:44 -0500

    added initial files

---
+++ js.js
@@ -0,0 +1,10 @@
+var myButton = document.getElementById("btn");
+var reveal = document.getElementById("show-item");
+var myHeadline = document.getElementById("headline1");
+
+myButton.addEventListener("click", function() {
+	$("#show-item").toggle();
+
+});
+
+
